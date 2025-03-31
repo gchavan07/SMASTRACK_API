06027fed3b903c809b26ea246689b0bb73028628
Gaurav Chavan <chavangaurav747234@gmail.com>
2025-03-31 16:16:11 +0530

    Added project files

---
+++ src/test/java/com/jbk/api/SamsTrackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jbk.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+@SpringBootTest(classes = com.tka.sams.api.SamsTrackApplication.class)
+class SamsTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
